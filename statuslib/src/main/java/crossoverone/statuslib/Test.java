f9fe3de18a1600771171c443e9a99e2275156082
crossoverone <18777696730@qq.com>
2018-01-15 14:17:41 +0800

    prepare

---
+++ statuslib/src/main/java/crossoverone/statuslib/Test.java
@@ -0,0 +1,12 @@
+package crossoverone.statuslib;
+
+/**
+ * Created by Administrator on 2018/1/15.
+ */
+
+public class Test {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+}
