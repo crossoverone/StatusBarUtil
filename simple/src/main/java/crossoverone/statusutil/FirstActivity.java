f5d7855bc69fd9df64dbdd2fd5b8f557b3637d86
crossoverone <18777696730@qq.com>
2018-01-15 18:32:41 +0800

    1.move

---
+++ simple/src/main/java/crossoverone/statusutil/FirstActivity.java
@@ -0,0 +1,18 @@
+package crossoverone.statusutil;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2018/1/15.
+ */
+
+public class FirstActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
--- simple/src/main/java/crossoverone/statusutil/MainActivity.java
+++ simple/src/main/java/crossoverone/statusutil/MainActivity.java
@@ -1,7 +1,9 @@
 package crossoverone.statusutil;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btn1(View view) {
+        startActivity(new Intent(this, FirstActivity.class));
     }
 }
